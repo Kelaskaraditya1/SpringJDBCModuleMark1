aa7931164629f8bd1604e08783a6f641bb547819
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-12-26 10:38:59 +0530

    Initial Commit

---
+++ src/main/java/com/StarkIndustries/SpringJDBCModuleMark1/SpringJdbcModuleMark1Application.java
@@ -0,0 +1,14 @@
+package com.StarkIndustries.SpringJDBCModuleMark1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class SpringJdbcModuleMark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcModuleMark1Application.class, args);
+	}
+	
+}
---
+++ src/test/java/com/StarkIndustries/SpringJDBCModuleMark1/SpringJdbcModuleMark1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StarkIndustries.SpringJDBCModuleMark1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcModuleMark1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
